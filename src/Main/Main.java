96233c51a317ff14be838ff4e98f30a04d6b60d1
cyred <kassuk91@gmail.com>
2016-10-14 21:27:38 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("IT IS USS TIME!");
         System.out.println("Autor: Ingvar Kassuk");
-        System.out.println("aasta: 2016");
+        System.out.println("Aasta: 2016");
     }
 }
